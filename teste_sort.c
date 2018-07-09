e8c0968ee46d549b5a1550d0109e43dd16272327
Rafael Escaleira <40582668+rafaelescaleira@users.noreply.github.com>
2018-07-08 23:41:38 -0400

    Contém função de teste

---
+++ teste_sort.c
@@ -0,0 +1,19 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                               *
+ * Nome : Rafael Escaleira Ferreira dos Santos.  *
+ * Universidade Federal do Mato Grosso do Sul    *
+ *                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <stdio.h>
+#include "sort.h"
+
+/* * * * * * * * * * * * Função Principal * * * * * * * * * * * */
+
+int main (void) {
+
+	confere_funcionamento (5000);
+    
+    return 0;
+    
+}
